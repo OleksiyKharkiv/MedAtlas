58bf3a8e93fe58fc6b015bef22d537069bebc188
OleksiyKhudoshyn <hudoshin7605@gmail.com>
2024-06-25 11:53:29 +0200

    Testing before deploy in AWS cloud 1.8

--- src/main/java/com/example/medatlas/controller/UserController.java
+++ src/main/java/com/example/medatlas/controller/UserController.java
@@ -1,71 +1,7 @@
-package com.example.medatlas.controller;
-
-import com.example.medatlas.dto.AuthRequest;
-import com.example.medatlas.dto.UserDTO;
-import com.example.medatlas.model.User;
-import com.example.medatlas.service.JwtService;
-import com.example.medatlas.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/api")
-public class UserController {
-
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private JwtService jwtService;
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @GetMapping("/isRunning")
-    @PreAuthorize("permitAll()")
-    public String isRunning() {
-        return "Service is running";
-    }
-
-    @PostMapping("/addUser")
-    public String addUser(@RequestBody UserDTO userDTO) {
-        userService.addUser(userDTO); // Использование UserService с UserDTO
-        return "User added successfully";
-    }
-
-    @GetMapping("/getAllUsers")
-    @PreAuthorize("hasRole('admin')")
-    public List<UserDTO> getAllUsers() {
-        return userService.getAllUsers(); // Использование UserService с UserDTO
-    }
-
-    @GetMapping("/{id}")
-    @PreAuthorize("hasRole('admin')")
-    public UserDTO getUserById(@PathVariable long id) {
-        return userService.getUserById(id); // Использование UserService с UserDTO
-    }
-
-    @PostMapping("/getToken")
-    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword()));
-
-        if (authentication.isAuthenticated()) {
-            return jwtService.generateToken(authRequest.getUserName());
-        }
-        throw new UsernameNotFoundException("Invalid user details.");
-    }
-}
-//
 //package com.example.medatlas.controller;
 //
 //import com.example.medatlas.dto.AuthRequest;
+//import com.example.medatlas.dto.UserDTO;
 //import com.example.medatlas.model.User;
 //import com.example.medatlas.service.JwtService;
 //import com.example.medatlas.service.UserService;
@@ -84,7 +20,7 @@ public class UserController {
 //public class UserController {
 //
 //    @Autowired
-//    private UserService service;
+//    private UserService userService;
 //
 //    @Autowired
 //    private JwtService jwtService;
@@ -99,20 +35,22 @@ public class UserController {
 //    }
 //
 //    @PostMapping("/addUser")
-//    public String addUser(@RequestBody User user) {
-//        return service.addUser(user);
+//    @PreAuthorize("permitAll()")
+//    public String addUser(@RequestBody UserDTO userDTO) {
+//        userService.addUser(userDTO); // Использование UserService с UserDTO
+//        return "User added successfully";
 //    }
 //
 //    @GetMapping("/getAllUsers")
 //    @PreAuthorize("hasRole('admin')")
-//    public List<User> getAllUsers() {
-//        return service.getAllUsers();
+//    public List<UserDTO> getAllUsers() {
+//        return userService.getAllUsers(); // Использование UserService с UserDTO
 //    }
 //
 //    @GetMapping("/{id}")
 //    @PreAuthorize("hasRole('admin')")
-//    public User getUserById(@PathVariable int id) {
-//        return service.getUser(id);
+//    public UserDTO getUserById(@PathVariable long id) {
+//        return userService.getUserById(id); // Использование UserService с UserDTO
 //    }
 //
 //    @PostMapping("/getToken")
@@ -122,6 +60,69 @@ public class UserController {
 //        if (authentication.isAuthenticated()) {
 //            return jwtService.generateToken(authRequest.getUserName());
 //        }
-//        throw new UsernameNotFoundException("invalid user details.");
+//        throw new UsernameNotFoundException("Invalid user details.");
 //    }
 //}
+//
+package com.example.medatlas.controller;
+
+import com.example.medatlas.dto.AuthRequest;
+import com.example.medatlas.model.User;
+import com.example.medatlas.service.JwtService;
+import com.example.medatlas.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @GetMapping("/isRunning")
+    @PreAuthorize("permitAll()")
+    public String isRunning() {
+        return "Service is running";
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(@RequestBody User user) {
+        return service.addUser(user);
+    }
+
+    @GetMapping("/getAllUsers")
+    @PreAuthorize("hasRole('admin')")
+    public List<User> getAllUsers() {
+        return service.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('admin')")
+    public User getUserById(@PathVariable int id) {
+        return service.getUser(id);
+    }
+
+    @PostMapping("/getToken")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword()));
+
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUserName());
+        }
+        throw new UsernameNotFoundException("invalid user details.");
+    }
+}
--- src/main/java/com/example/medatlas/service/UserService.java
+++ src/main/java/com/example/medatlas/service/UserService.java
@@ -1,47 +1,7 @@
-package com.example.medatlas.service;
-
-import com.example.medatlas.dto.UserDTO;
-import com.example.medatlas.mapper.UserMapper;
-import com.example.medatlas.model.User;
-import com.example.medatlas.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class UserService {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private UserMapper userMapper; // Использование UserMapper для преобразования между User и UserDTO
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    public List<UserDTO> getAllUsers() {
-        List<User> users = userRepository.findAll();
-        return userMapper.toDTOList(users); // Преобразование списка User в список UserDTO
-    }
-
-    public UserDTO getUserById(long id) {
-        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
-        return userMapper.toDTO(user); // Преобразование User в UserDTO
-    }
-
-    public void addUser(UserDTO userDTO) {
-        User user = userMapper.toEntity(userDTO); // Преобразование UserDTO в User
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRepository.save(user);
-    }
-}
-//
 //package com.example.medatlas.service;
 //
+//import com.example.medatlas.dto.UserDTO;
+//import com.example.medatlas.mapper.UserMapper;
 //import com.example.medatlas.model.User;
 //import com.example.medatlas.repository.UserRepository;
 //import org.springframework.beans.factory.annotation.Autowired;
@@ -55,30 +15,70 @@ public class UserService {
 //public class UserService {
 //
 //    @Autowired
-//    private UserRepository repository;
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private UserMapper userMapper; // Использование UserMapper для преобразования между User и UserDTO
 //
 //    @Autowired
 //    private PasswordEncoder passwordEncoder;
 //
-//    public List<User> getAllUsers() {
-//        return repository.findAll();
+//    public List<UserDTO> getAllUsers() {
+//        List<User> users = userRepository.findAll();
+//        return userMapper.toDTOList(users); // Преобразование списка User в список UserDTO
 //    }
 //
-//    public User getUser(long id) {
-//        Optional<User> userInfo =  repository.findById(id);
-//
-//        if (userInfo.isPresent()) {
-//            return userInfo.get();
-//        }
-//
-//        throw new RuntimeException("User details not found for id " + id);
+//    public UserDTO getUserById(long id) {
+//        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+//        return userMapper.toDTO(user); // Преобразование User в UserDTO
 //    }
 //
-//    public String addUser(User user) {
-//        System.out.println("Received user: " + user.toString());
+//    public void addUser(UserDTO userDTO) {
+//        User user = userMapper.toEntity(userDTO); // Преобразование UserDTO в User
 //        user.setPassword(passwordEncoder.encode(user.getPassword()));
-//        repository.save(user);
-//        System.out.println("User saved: " + user);
-//        return "user added to system ";
+//        userRepository.save(user);
 //    }
 //}
+//
+package com.example.medatlas.service;
+
+import com.example.medatlas.model.User;
+import com.example.medatlas.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public List<User> getAllUsers() {
+        return repository.findAll();
+    }
+
+    public User getUser(long id) {
+        Optional<User> userInfo =  repository.findById(id);
+
+        if (userInfo.isPresent()) {
+            return userInfo.get();
+        }
+
+        throw new RuntimeException("User details not found for id " + id);
+    }
+
+    public String addUser(User user) {
+        System.out.println("Received user: " + user.toString());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        repository.save(user);
+        System.out.println("User saved: " + user);
+        return "user added to system ";
+    }
+}
